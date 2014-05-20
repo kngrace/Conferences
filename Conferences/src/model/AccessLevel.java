27667c707d2627e33bf04c71c043ff975673b90f
trevorjennings <trevorjennings@d-69-91-207-206.dhcp4.washington.edu>
2014-05-20 12:01:07 -0700

    Added classes AccessLevel, Conference, DateTime, Manuscript, Session, Status

--- Conferences/src/model/AccessLevel.java
+++ Conferences/src/model/AccessLevel.java
@@ -1,10 +1,13 @@
 package model;
+import java.util.Comparator;
 
-public enum AccessLevel {
-
-	//dummy enum
-	
-	AccessLevel(){
-		
+public enum AccessLevel implements Comparator<AccessLevel> {
+	AUTHOR(0), REVIEWER(0), SUBPROGRAMCHAIR(1), PROGRAMCHAIR(2);
+	private int value;
+	private AccessLevel(int theValue) {
+		value = theValue;
+	}
+	public int compare(AccessLevel al1, AccessLevel al2) {
+		return al1.value - al2.value;
 	}
 }
