cd36aa88c43f03c296b41bbd79c922216c49ded0
trevorjennings <trevorjennings@192.168.1.3>
2014-06-02 14:14:00 -0700

    Adding updated Manuscript, AccessLevel and Review.

--- Conferences/src/model/AccessLevel.java
+++ Conferences/src/model/AccessLevel.java
@@ -1,13 +1,89 @@
+/**
+ * TCSS 360 Software Development and Quality Assurance
+ * Conferences Project - Group 3
+ */
+
 package model;
+
 import java.util.Comparator;
 
+/**
+ * AccessLevel is an Enum to enumerate the hierarchy of access levels
+ * particular Users have for particular Conferences. The access levels
+ * vary in number between integers 0 and 2 but have been described here
+ * as enumerations for code readability. 
+ * @author Trevor Jennings
+ * @version 2 June 2014
+ */
 public enum AccessLevel implements Comparator<AccessLevel> {
+	
+	/**
+	 * Enum "constructors" assign integer values to each enumeration
+	 */
 	AUTHOR(0), REVIEWER(0), SUBPROGRAMCHAIR(1), PROGRAMCHAIR(2);
-	private int value;
+	
+	/**
+	 * The enumeration's integer value.
+	 */
+	private int myValue;
+	
+	/**
+	 * Constructor creates new AccessLevel with integer value being
+	 * passed as parameter. Will not likely need to be used for
+	 * the Conferences project.
+	 * @param theValue the enum's value.
+	 */
 	private AccessLevel(int theValue) {
-		value = theValue;
+		myValue = theValue;
 	}
+	
+	public int getValue() {
+		return myValue;
+	}
+	
+	/**
+	 * AccessLevel implements Comparable interface. Compare's two
+	 * enum's integer values, returning the difference between the
+	 * first and second parameters.
+	 * @param al1 the first AccessLevel to be compared.
+	 * @param al2 the second AccessLevel to be compared.
+	 * @return the difference between the two parameter's values.
+	 */
 	public int compare(AccessLevel al1, AccessLevel al2) {
-		return al1.value - al2.value;
+		return al1.myValue - al2.myValue;
+	}
+    
+	/**
+	 * Returns the AccessLevel of the passed integer value. If the integer
+	 * value does not match an existing AccessLevel, the lowest AccessLevel
+	 * (integer value 0, AccessLevel.AUTHOR) is returned.
+	 * @param theValue the integer value of an existing AccessLevel.
+	 * @return the AccessLevel of the passed integer value.
+	 */
+	public static AccessLevel valueOf(int theValue) {
+		switch (theValue) {
+		    case 0 : return AccessLevel.AUTHOR;
+		    case 1 : return AccessLevel.SUBPROGRAMCHAIR;
+		    case 2 : return AccessLevel.PROGRAMCHAIR;
+		    default : return AccessLevel.AUTHOR;
+		}
+	}
+	
+	/**
+	 * Returns the integer value of the passed AccessLevel. If the
+	 * AccessLevel does not exist, the lowest integer value for
+	 * AccessLevel's is returned (0). 
+	 * @param theAccessLevel the AccessLevel who's integer value is 
+	 * to be returned.
+	 * @return the integer value of the passed AccessLevel.
+	 */
+	public static int accessLevelOf(AccessLevel theAccessLevel) {
+		switch (theAccessLevel) {
+		    case AUTHOR : return 0;
+		    case REVIEWER : return 0;
+		    case SUBPROGRAMCHAIR : return 1;
+		    case PROGRAMCHAIR : return 2;
+		    default : return 0;
+		}
 	}
 }
