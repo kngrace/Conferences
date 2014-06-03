26890b6f297860f5572b5579519216b0576c6c2b
trevorjennings <trevorjennings@d-205-175-113-55.dhcp4.washington.edu>
2014-06-03 11:13:46 -0700

    Added features to class Status.

--- Conferences/src/model/Status.java
+++ Conferences/src/model/Status.java
@@ -1,12 +1,35 @@
 package model;
 
 public enum Status {
+	
 	APPROVED(2), UNDECIDED(1), REJECTED(0);
+	
 	private int myValue;
 	
-	
-	public Status(int theValue) {
+	private Status(int theValue) {
 		myValue = theValue;
+	}
+	
+	public int getValue() {
+		return myValue;
+	}
+	
+	public Status getStatus(int theValue) throws Exception {
+		switch (theValue) {
+		    case 0 : return Status.REJECTED;
+		    case 1 : return Status.UNDECIDED;
+		    case 2 : return Status.APPROVED;
+		    default : throw new Exception("Invalid Input!");
+		}
+	}
+	
+	public int getValue(Status theStatus) throws Exception {
+		switch (theStatus) {
+		    case REJECTED : return 0;
+		    case UNDECIDED : return 1;
+		    case APPROVED : return 2;
+		    default : throw new Exception("Invalid Input!");
+		}
 	}
 	
 }
