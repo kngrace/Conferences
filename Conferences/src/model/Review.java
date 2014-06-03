d2022db1d24ad94abf792d5893962f84f8272d89
Eric <absolute000@gmail.com>
2014-06-03 11:54:35 -0700

    Added getters and setters and updated constructor with new fields.

--- Conferences/src/model/Review.java
+++ Conferences/src/model/Review.java
@@ -1,18 +1,59 @@
 package model;
 
+import java.io.File;
+
+import control.ManuscriptControl;
+
 public class Review {
-	private int myID;
-	private User myReviewer;
+	private final int myID;
+	private final User myReviewer;
 	private String myFileName;
-	public Review(int theID, User theReviewer, String theFileName) {
+	private File myFile;
+	private final Manuscript myManuscript;
+	
+	public Review(final int theID, final User theReviewer, final String theFileName,
+			final File theFile, final Manuscript theManuscript) {
 		myID = theID;
 		myReviewer = theReviewer;
 		myFileName = theFileName;
+		myFile = theFile;
+		myManuscript = theManuscript;
 	}
-	public Review(User theReviewer, String theFileName, Session theSession) {
+	
+	public Review(final User theReviewer, final String theFileName, 
+			final File theFile, final Manuscript theManuscript) {
 		myReviewer = theReviewer;
 		myFileName = theFileName;
+		myFile = theFile;
+		myManuscript = theManuscript;
 		myID = ManuscriptControl.createReview(this);
 	}
 	
+	public int getID() {
+		return myID;
+	}
+	
+	public User getReviewer() {
+		return myReviewer;
+	}
+	
+	public String getFileName() {
+		return myFileName;
+	}
+	
+	public File getFile() {
+		return myFile;
+	}
+	
+	public Manuscript getManuscript() {
+		return myManuscript;
+	}
+	
+	public void setFileName(final String theFile) {
+		myFileName = theFile;
+	}
+	
+	public void setFile(final File theFile) {
+		myFile = theFile;
+	}
 }
