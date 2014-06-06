2d58491a84d1e8f168cdba0929a2688b92f1417c
trevorjennings <trevorjennings@192.168.1.3>
2014-06-06 12:54:03 -0700

    Syncing with Kirsten

--- Conferences/src/model/TestManuscript.java
+++ Conferences/src/model/TestManuscript.java
@@ -6,6 +6,8 @@ import java.io.File;
 import java.sql.Date;
 import java.util.List;
 
+import java.io.FileNotFoundException;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.Rule;
@@ -21,6 +23,8 @@ public class TestManuscript {
 	private User myUser1;
 	private User myUser2;
 	private Manuscript myManuscript1;
+	private Manuscript myManuscript2;
+	private Manuscript myManuscript3;
 	private Conference myConference1;
 	private Review myReview1;
 	private Review myReview2;
@@ -29,28 +33,43 @@ public class TestManuscript {
 
 	@Before
 	public void setUp() throws Exception {
-		myUser1 = new User(0001, "tjennz12", "trevorcarljennings", "tcj12@gmail.com", 
-			"Trevor", "Jennings", "123 N Lane Dr, Tacoma, WA 98406");
-		myUser2 = new User(0014, "jocuz1018", "jor18", "jocuz1018@gmail.com", null, 
+		myUser1 = new User(randomString(), "trevorcarljennings", "tcj12@gmail.com", 
+			"Trevor", "Jennings", "123 N Lane Dr, Tacoma, WA 98406"); 
+		myUser2 = new User(randomString(), "jor18", "jocuz1018@gmail.com", "Jordan", 
 				"Jennings", null);
-		myConference1 = new Conference(0020, "Conference A", myUser1, Date.valueOf("2015-04-13"),
+		myConference1 = new Conference("Conference A", myUser1, Date.valueOf("2015-04-13"),
 				Date.valueOf("2015-05-13"), Date.valueOf("2015-05-20"), 
 				Date.valueOf("2015-05-22"), "Tacoma, WA", "Description for Conference1");
-		myManuscript1 = new Manuscript(0012, myUser1, myConference1, "sample1.txt",
-			new File("sample1.txt"), Status.APPROVED, Status.REJECTED, true);
-		myReview1 = new Review(0040, myUser2, "myReview1.txt", new File("myReview1.txt"), 
+		myManuscript1 = new Manuscript(myUser1, myConference1, "sample1.txt", 
+				new File("sample1.txt"));
+		myManuscript2 = new Manuscript(myUser2, myConference1, "sample2.txt",
+				new File("sample2.txt"));
+		myManuscript3 = new Manuscript(myUser2, myConference1, null, new File("sample3.txt"));
+		myReview1 = new Review(myUser2, "review1.txt", new File("review1.txt"), 
 				myManuscript1);
 		//myUser2.setAccess(myConference1, AccessLevel.AUTHOR);
 		//myUser1.setAccess(myConference1, AccessLevel.PROGRAMCHAIR);
 		mySession1 = new Session(myUser1);
 		mySession2 = new Session(myUser2);
+		myManuscript2.submit(mySession2);
 		
 	}
+	
+	public static String randomString() {
+		StringBuilder sb = new StringBuilder(10);
+		for (int i = 0; i < 20; i++) {
+			char c = (char) ((57 * Math.random()) + 65);
+			sb.append(c);
+		}
+		return sb.toString();
+	}
+	
 
 	@Test
 	public void testGetFinalStatus() {
-		assertEquals("Final status should be rejected.", myManuscript1.getFinalStatus(mySession1), 
-				Status.REJECTED);
+		myManuscript1.setFinalStatus(Status.APPROVED, mySession1);
+		assertEquals("Final status should be APPROVED.", myManuscript1.getFinalStatus(mySession1), 
+				Status.APPROVED);
 	}
 	
 	@Rule
@@ -59,11 +78,33 @@ public class TestManuscript {
 	@Test
 	public void testGetFinalStatusException() {
 		exception.expect(IllegalStateException.class);
-		// mySession2 only has Author access for myConference1 which myManuscript1 is part of.
+		myManuscript1.setFinalStatus(Status.REJECTED, mySession2);
+		// mySession2 only has the lowest access for myConference1 which myManuscript1 is part of.
 		// Thus, an exception should be thrown.
 		myManuscript1.getFinalStatus(mySession2);
 	}
 	
+	@Test
+	public void testSubmit() {
+		myManuscript1.setIsSubmitted(false);
+		try {
+		    myManuscript1.submit(mySession1);
+		} catch (Exception e) {
+			System.err.println("File does not exist!");
+		}
+		assertTrue("Manuscript should now be submitted.", myManuscript1.isSubmitted());
+		exception.expect(Exception.class);
+		try {
+			myManuscript3.submit(mySession1);
+		} catch (FileNotFoundException e) {
+			System.err.println("File does not exist!");
+		} catch (Exception e) {
+			System.err.println("Exception was THROWN: " + mySession1.getCurrentUser() + " is not " + myManuscript3.getAuthor());
+		}	
+	}
 	
-
+	@Test
+	public void testUnsubmit() {
+		
+	}
 }
