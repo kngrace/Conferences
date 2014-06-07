78c3cc53ff07a3f6148a4c417531317afcfc4a29
nikhila-potharaj <pothnik@uw.edu>
2014-06-06 19:31:31 -0700

    Update UserScreen.java

--- Conferences/src/view/UserScreen.java
+++ Conferences/src/view/UserScreen.java
@@ -1,35 +1,50 @@
 package view;
 
-import java.awt.Color;
+import java.util.Observable;
+import java.util.Observer;
 
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
-import control.ConferenceControl;
 import model.AccessLevel;
 import model.Conference;
 import model.Session;
+import control.ConferenceControl;
 
+/**
+ * The User screen for a particular conference. 
+ * 
+ * @author Nikhila Potharaj
+ * @version 06.06.2014
+ *
+ */
+public class UserScreen implements Observer {
 
-public class UserScreen {
+	/**
+	 * TODO - Placeholder for the reviewer tab inside a conference. 
+	 */
+	private JPanel myReviewerTab;
 
-	private JFrame frame;
-	private JPanel AuthorTab;
-	private JPanel ReviewTab;
-	private JPanel SPCTab;
-	private JPanel PCTab;
-	private JPanel panel_2;
-	private JPanel panel_3;
-	private JPanel panel_4;
-	
+	/**
+	 * The conference for this screen. 
+	 */
 	private Conference myConference;
+
+	/**
+	 * The session for the user that is currently using the system. 
+	 */
 	private Session mySession;
-	
-	private JTabbedPane tabbedPane;
+
+	/**
+	 * The tabbedpane containing all the tabs that a user can view. 
+	 */
+	private JTabbedPane myTabbedPane;
 
 	/**
 	 * Create the application.
+	 * 
+	 * @param the_conference to be assigned to myConference
+	 * @param the_session to be assigned to mySession
 	 */
 	public UserScreen(Conference the_conference, Session the_session) {
 		myConference = the_conference;
@@ -41,109 +56,60 @@ public class UserScreen {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.getContentPane().setBackground(new Color(176, 196, 222));
-		frame.setBounds(100, 100, 699, 467);
-		frame.getContentPane().setLayout(null);
-		
-		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(0, 0, 536, 367);
-		frame.getContentPane().add(tabbedPane);
-		
-		JPanel DefaultTab = new JPanel();
-		tabbedPane.addTab("Default ", null, new DefaultTab(myConference, mySession).getPanel(), null);
-		DefaultTab.setLayout(null);
-		
-		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 531, 318);
-		//DefaultTab.add(panel);
-		panel.setLayout(null);
-		panel.add(new DefaultTab(myConference, mySession).getPanel());
 
-		AuthorTab = new JPanel();
-		AuthorTab.setBackground(new Color(135, 206, 235));
-		AuthorTab.setToolTipText("");
-		tabbedPane.addTab("Author", null, AuthorTab, null);
-		AuthorTab.setLayout(null);
+		myTabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		myTabbedPane.setBounds(0, 0, 536, 367);
+		myReviewerTab = new JPanel();
+		myReviewerTab.setBounds(0, 0, 537, 318);
+
+		//Default tab before the user is an author. 
+		myTabbedPane.addTab("Default ", null, new DefaultTab(myConference, mySession).getPanel(), null);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(0, 0, 531, 318);
-		AuthorTab.add(panel_1);
-		panel_1.setLayout(null);
-		
-		ReviewTab = new JPanel();
-		ReviewTab.setBackground(new Color(255, 255, 0));
-		ReviewTab.setLayout(null);
-		
-		panel_2 = new JPanel();
-		panel_2.setBounds(0, 0, 531, 318);
-		ReviewTab.add(panel_2);
-		panel_2.setLayout(null);
-		
-		SPCTab = new JPanel();
-		SPCTab.setBackground(new Color(255, 215, 0));
-		
-		SPCTab.setLayout(null);
-		
-		panel_3 = new JPanel();
-		panel_3.setBounds(0, 0, 531, 318);
-		SPCTab.add(panel_3);
-		panel_3.setLayout(null);
-		
-		PCTab = new JPanel();
-		PCTab.setBackground(new Color(255, 165, 0));
-		
-		PCTab.setLayout(null);
-		
-		panel_4 = new JPanel();
-		panel_4.setBounds(0, 0, 531, 318);
-		PCTab.add(panel_4);
-		panel_4.setLayout(null);
-		
-		
+		//By the access level, the panes are either visible or they are not. 
 		AccessLevel access = ConferenceControl.getAccessLevel(myConference, mySession.getCurrentUser()); 
 		System.out.println(access);
 		if(access != null) {
-			if(access == AccessLevel.AUTHOR) {
+			if(access == AccessLevel.AUTHOR) { // when the user is an author
 				//AuthorTab only
-				tabbedPane.addTab("Author", null, 
+				myTabbedPane.addTab("Author", null, 
 						new AuthorTab(myConference, mySession).getPanel(), null);	
-			} else if(access == AccessLevel.REVIEWER) {
-				tabbedPane.addTab("Author", null, 
+			} else if(access == AccessLevel.REVIEWER) { // when access level is up to a reviewer
+				myTabbedPane.addTab("Author", null, 
 						new AuthorTab(myConference, mySession).getPanel(), null);	
-				tabbedPane.addTab("Reviewer", null, ReviewTab, null);
-			} else if(access == AccessLevel.SUBPROGRAMCHAIR) {
-				tabbedPane.addTab("Author", null, 
+				myTabbedPane.addTab("Reviewer", null, myReviewerTab, null);
+			} else if(access == AccessLevel.SUBPROGRAMCHAIR) { // access level to an SPC
+				myTabbedPane.addTab("Author", null, 
 						new AuthorTab(myConference, mySession).getPanel(), null);	
-				tabbedPane.addTab("Reviewer", null, ReviewTab, null);
-				tabbedPane.addTab("Sub-Program Chair", null, SPCTab, null);
-			} else if(access == AccessLevel.PROGRAMCHAIR) {
-				tabbedPane.addTab("Author", null, 
+				myTabbedPane.addTab("Reviewer", null, myReviewerTab, null);
+				myTabbedPane.addTab("Sub-Program Chair", null, 
+						new SPCTab(myConference, mySession).getPanel(), null);
+			} else if(access == AccessLevel.PROGRAMCHAIR) { //access level of a PC
+				myTabbedPane.addTab("Author", null, 
 						new AuthorTab(myConference, mySession).getPanel(), null);	
-				tabbedPane.addTab("Reviewer", null, ReviewTab, null);
-				tabbedPane.addTab("Sub-Program Chair", null, SPCTab, null);
-				tabbedPane.addTab("Program Chair", null, 
+				myTabbedPane.addTab("Reviewer", null, myReviewerTab, null);
+				myTabbedPane.addTab("Sub-Program Chair", null, 
+						new SPCTab(myConference, mySession).getPanel(), null);
+				myTabbedPane.addTab("Program Chair", null, 
 						new PCTab(myConference, mySession).getPanel(), null);
 			}
 		}
-		
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	
+
+	/**
+	 * Returns the tabbed pane containing all of the tabs, back to the window. 
+	 * 
+	 * @return myTabbedPane
+	 */
 	public JTabbedPane getTab() {
-		return tabbedPane;
+		return myTabbedPane;
 	}
-	
-	public JPanel getAuthorTab() {
-		return AuthorTab;
-	}
-	public JPanel getReviewTab() {
-		return ReviewTab;
-	}
-	public JPanel getSPCTab() {
-		return SPCTab;
-	}
-	public JPanel getPCTab() {
-		return PCTab;
+
+	/**
+	 * TODO still need to figure out the dynamic changes for all the tabs.
+	 */
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		System.out.println("changes");
+
 	}
 }
